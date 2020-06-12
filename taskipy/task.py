495db74ae30c0d671f1ecacceab5a09634b40bd6
Eugene Triguba <eugenetriguba@gmail.com>
2020-06-12 15:21:55 -0500

    Add docstrings & types to task & task_runner.
    Also, a small refactoring of __bail_on_first_fail()
    in task_runner.py.

--- taskipy/task.py
+++ taskipy/task.py
@@ -1,9 +1,23 @@
+from typing import Optional
+
 from taskipy.exceptions import InvalidRunnerTypeError, TaskNotFoundError
 from taskipy.pyproject import PyProject
 
 
 class Task:
     def __init__(self, task_name: str):
+        """
+        Sets up the task by saving it's name and
+        retrieiving the command it references in the
+        pyproject file.
+
+        Args:
+            task_name (str): The name of the task in [tools.taskipy.tasks].
+
+        Raises:
+            TaskNotFoundError: If the task could not be found in the
+                               [tools.taskipy.tasks] section.
+        """
         self.project = PyProject()
         self.name = task_name
 
@@ -12,25 +26,47 @@ class Task:
         except KeyError:
             raise TaskNotFoundError(self.name)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
     @property
-    def pre_task(self):
+    def pre_task(self) -> Optional[str]:
+        """
+        Retrieves the pre task of the given task.
+
+        Returns:
+            str or None: The pre task, if it exists.
+        """
         try:
             return self.project.tasks[f"pre_{self.name}"]
         except KeyError:
             return None
 
     @property
-    def post_task(self):
+    def post_task(self) -> Optional[str]:
+        """
+        Retrieves the post task of the given task.
+
+        Returns:
+            str or None: The post task, if it exists.
+        """
         try:
             return self.project.tasks[f"post_{self.name}"]
         except KeyError:
             return None
 
     @property
-    def runner(self):
+    def runner(self) -> Optional[str]:
+        """
+        Retrieves what runner to use for this task
+        from [tools.taskipy.settings.runner].
+
+        Raises:
+            InvalidRunnerTypeError: If the type of the runner value is not a string.
+
+        Returns:
+            str or None: The runner to use, if it is set.
+        """
         try:
             return self.project.settings["runner"].strip()
         except KeyError:
--- taskipy/task_runner.py
+++ taskipy/task_runner.py
@@ -8,25 +8,53 @@ from taskipy.task import Task
 
 class TaskRunner:
     def __init__(self, task_name: str, args: List[str]):
+        """
+        Task Runner is what handles running the task from the cli arguments.
+
+        It is initialized by retrieving the command to run from the given task_name
+        and adding that commands, with any arguments, into the commands to run later.
+
+        Args:
+            task_name (str): The name of the task to run from [tools.taskipy.tasks].
+            args (List[str]): The arguments passed in to that command.
+        """
         self.task = Task(task_name)
         self.commands = []
         self.commands.append(
             " ".join([self.task.command] + [shlex.quote(arg) for arg in args])
         )
 
-    def run(self):
+    def run(self) -> int:
+        """
+        Sets up the order of the commands to be
+        executed if there are any pre/post tasks
+        and then runs the commands, bailing on the
+        first fail.
+
+        Returns:
+            int: The return code of the last task that ran.
+                 0 if all tasks succeeded; > 0 on fail.
+        """
         if self.task.pre_task:
             self.commands.insert(0, self.task.pre_task)
 
         if self.task.post_task:
             self.commands.append(self.task.post_task)
 
-        return self.__bail_on_first_fail(self.commands, runner=self.task.runner)
+        return self.__run_commands_and_bail_on_first_fail()
 
-    def __bail_on_first_fail(self, cmds: List[str], runner: str = None) -> int:
-        for cmd in cmds:
-            if runner is not None:
-                cmd = f"{runner} {cmd}"
+    def __run_commands_and_bail_on_first_fail(self) -> int:
+        """
+        Runs the commands and returns a non-zero
+        exit code on the first fail.
+
+        Returns:
+            int: The return code of the last task that ran.
+                 0 if all tasks succeeded; > 0 on fail.
+        """
+        for cmd in self.commands:
+            if self.task.runner is not None:
+                cmd = f"{self.task.runner} {cmd}"
 
             process = subprocess.Popen(cmd, shell=True, cwd=Path.cwd())
 
