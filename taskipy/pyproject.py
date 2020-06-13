18add6c256ce46c6450300f1253c8a5bff690b2f
Eugene Triguba <eugenetriguba@gmail.com>
2020-06-13 14:46:24 -0500

    Make path & items in PyProject private

--- taskipy/pyproject.py
+++ taskipy/pyproject.py
@@ -13,26 +13,26 @@ from taskipy.exceptions import (
 
 class PyProject:
     def __init__(self, file_name: str = "pyproject.toml", directory: Path = Path.cwd()):
-        self.path = Path(directory / file_name).resolve()
-        self.items = self.__load_toml_file()
+        self.__path = Path(directory / file_name).resolve()
+        self.__items = self.__load_toml_file()
 
     @property
     def tasks(self) -> dict:
         try:
-            return self.items["tool"]["taskipy"]["tasks"]
+            return self.__items["tool"]["taskipy"]["tasks"]
         except KeyError:
             raise MissingTaskipyTasksSectionError()
 
     @property
     def settings(self) -> dict:
         try:
-            return self.items["tool"]["taskipy"]["settings"]
+            return self.__items["tool"]["taskipy"]["settings"]
         except KeyError:
             raise MissingTaskipySettingsSectionError()
 
     def __load_toml_file(self) -> MutableMapping[str, Any]:
         try:
-            return toml.load(self.path)
+            return toml.load(self.__path)
         except FileNotFoundError:
             raise MissingPyProjectFileError()
         except toml.TomlDecodeError:
