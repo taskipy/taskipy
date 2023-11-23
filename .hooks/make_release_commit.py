923fb72dc1e4e83ee1b504d49517323d9d821036
Roy Sommer <illBeRoy@users.noreply.github.com>
2023-11-23 18:02:07 +0200

    Release Version 1.12.2 (#73)
    * change release script
    
    * small fix to release script
    
    * Release version 1.12.2

--- .hooks/make_release_commit.py
+++ .hooks/make_release_commit.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 import tomli
+import secrets
 import subprocess
 from pathlib import Path
 
+
 def create_release_commit():
     cwd = Path.cwd()
-    with open(cwd / 'pyproject.toml', 'rb') as file:
+    with open(cwd / "pyproject.toml", "rb") as file:
         pyproject = tomli.load(file)
-    version = pyproject['tool']['poetry']['version']
+    version = pyproject["tool"]["poetry"]["version"]
+    branch = f"release-{version}-{secrets.token_hex(12)}"
 
-    p = subprocess.Popen(f'git add . && git commit -m "Release version {version}" && git tag -a "{version}" -m "Release version {version}" && git push && git push --tags',
-                         shell=True,
-                         cwd=cwd)
+    p = subprocess.Popen(
+        " && ".join(
+            [
+                f"git checkout -b {branch}",
+                "git add .",
+                f'git commit -m "Release version {version}"',
+                f'git tag -a "{version}" -m "Release version {version}"',
+                f"git push --set-upstream origin {branch}",
+                f"git push --tags",
+            ]
+        ),
+        shell=True,
+        cwd=cwd,
+    )
 
     p.wait()
 
-if __name__ == '__main__':
-    create_release_commit()
 
+if __name__ == "__main__":
+    create_release_commit()
